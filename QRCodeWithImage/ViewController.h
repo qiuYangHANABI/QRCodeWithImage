c8f57371e82bb940c3618d69c7ad4b3886cec8ca
qiuYangHANABI <qiuyang_hanabi@163.com>
2018-10-08 11:19:33 +0800

    Initial Commit

---
+++ QRCodeWithImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeWithImage
+//
+//  Created by 黃秋陽 on 2018/10/8.
+//  Copyright © 2018 黃秋陽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRCodeWithImage/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QRCodeWithImage
+//
+//  Created by 黃秋陽 on 2018/10/8.
+//  Copyright © 2018 黃秋陽. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
